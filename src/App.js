6d77af30844c440395af01353c4f292c0102ecfb
Dhura Mistry <dhuramistry@gmail.com>
2023-12-29 18:42:13 -0500

    yes changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import React, { useState } from "react";
 
 function App() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  let [position, setPosition] = useState({ x: 0, y: 0 });
 
-  const handleMove = () => {
+  let [showYes, setshowYes] = useState(true);
+
+  let handleMove = () => {
     // Calculate new position
     const newX = Math.random() * window.innerWidth;
     const newY = Math.random() * window.innerHeight;
@@ -13,48 +15,53 @@ function App() {
     setPosition({ x: newX, y: newY });
   };
 
-  const handleYes = () => {};
+  let handleYes = () => {
+    setshowYes(false);
+  };
 
   return (
     <div className="App">
-      <div className="question">
-        <div>
-          <h1 className="font-bold text-rose-300 text-center mt-10">
-            Will you go out on a date with me?
-          </h1>
+      {showYes ? (
+        <div className="question">
+          <div>
+            <h1 className="font-bold text-rose-300 text-center mt-10">
+              Will you go out on a date with me?
+            </h1>
+          </div>
+          <div>
+            {/* <img src="https://media1.tenor.com/m/XZGm-kKjguMAAAAC/kawaii-bunny.gif" alt="pleasee" className="mx-auto bg-blend-overlay"/> */}
+            <img
+              src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3hjY2F3cGZ4NHI0aDAydGtzaDh3NDhoNWpxc205d2MwNjRzcnQ2dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SHdwS47nztG2MHCOTH/giphy.gif"
+              alt="pleasee"
+              className="mx-auto bg-blend-overlay size"
+            />
+          </div>
+          <div className="flex-auto text-center mt-10">
+            <button
+              onClick={handleYes}
+              className="p-3 bg-rose-300 rounded-xl font-bold text-white"
+            >
+              Yes
+            </button>
+            <button
+              onMouseEnter={handleMove}
+              style={{ top: position.y, left: position.x }}
+              className="moving-button p-3 bg-rose-300 ml-3 rounded-xl font-bold text-white"
+            >
+              No
+            </button>
+          </div>
         </div>
-        <div>
-          {/* <img src="https://media1.tenor.com/m/XZGm-kKjguMAAAAC/kawaii-bunny.gif" alt="pleasee" className="mx-auto bg-blend-overlay"/> */}
+      ) : (
+        <div className="answer">
+          <h1 className="font-bold text-rose-300 text-center mt-10">Yayyyyy, there's no going back now !</h1>
           <img
-            src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3hjY2F3cGZ4NHI0aDAydGtzaDh3NDhoNWpxc205d2MwNjRzcnQ2dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SHdwS47nztG2MHCOTH/giphy.gif"
+            src="https://media1.tenor.com/m/XZGm-kKjguMAAAAC/kawaii-bunny.gif"
             alt="pleasee"
-            className="mx-auto bg-blend-overlay size"
+            className="mx-auto bg-blend-overlay"
           />
         </div>
-        <div className="flex-auto text-center mt-10">
-          <button
-            onClick={handleYes}
-            className="p-3 bg-rose-300 rounded-xl font-bold text-white"
-          >
-            Yes
-          </button>
-          <button
-            onMouseEnter={handleMove}
-            style={{ top: position.y, left: position.x }}
-            className="moving-button p-3 bg-rose-300 ml-3 rounded-xl font-bold text-white"
-          >
-            No
-          </button>
-        </div>
-      </div>
-      <div className="answer">
-        <h1 className="font-bold text-rose-300 text-center mt-10">Yayyyyy</h1>
-        <img
-          src="https://media1.tenor.com/m/XZGm-kKjguMAAAAC/kawaii-bunny.gif"
-          alt="pleasee"
-          className="mx-auto bg-blend-overlay"
-        />
-      </div>
+      )}
     </div>
   );
 }
